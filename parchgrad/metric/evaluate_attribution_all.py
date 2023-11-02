ef50c3bfc9b7712d734fb9ca58ec840a6e450ffe
fxnnxc <bumjin@kaist.ac.kr>
2023-11-03 04:02:00 +0900

    update the shell types

--- parchgrad/metric/evaluate_attribution_all.py
+++ parchgrad/metric/evaluate_attribution_all.py
@@ -12,15 +12,17 @@ def evaluate_attribution_all(input,
                             ):
     
     # ------- evaluate logits -----------
+    results = {}
+    
     for order in ['morf' ,'lerf']:
         metric_aopc, metric_lodds, acc, metric_fracdiff = evaluate_attr_with_logits(input, label, attr, model, device, ratios, order,)
         
-    results = {
-        'aopc'      : {r:metric_aopc[i].item() for i, r in enumerate(ratios)},
-        'lodds'     : {r:metric_lodds[i].item() for i, r in enumerate(ratios)} ,
-        'acc'       : {r:int(acc[i]) for i, r in enumerate(ratios)} ,
-        'fracdiff'  : {r:metric_fracdiff[i].item() for i, r in enumerate(ratios)},
-    }
+        results.update({order:{
+            'aopc'      : {r:metric_aopc[i].item() for i, r in enumerate(ratios)},
+            'lodds'     : {r:metric_lodds[i].item() for i, r in enumerate(ratios)} ,
+            'acc'       : {r:int(acc[i]) for i, r in enumerate(ratios)} ,
+            'fracdiff'  : {r:metric_fracdiff[i].item() for i, r in enumerate(ratios)},
+        }})
         
     # ------- evaluate bbox -----------
         
--- shells/baseline/ig.sh
+++ shells/baseline/ig.sh
@@ -3,22 +3,17 @@
 data_path=/data/ImageNet1k
 bbox_path=/data/ILSVRC2012_bbox_val
 
-input_attrib=ig
-quantile=0.1
-alpha=1e-10 # not used because of quantile 
+input_attrib=grad
+quantile=1.0
+alpha=0.05 # not used because of quantile 
 p_value_threshold=0.05
-layer_ratio=0.9
-method=cls
+layer_ratio=1.0
 
-export MKL_NUM_THREADS=2
-export NUMEXPR_NUM_THREADS=2
-export OMP_NUM_THREADS=2
-
-
-for encoder in resnet18 vgg16 efficient_b0 # convnext_tiny
+for encoder in resnet18  efficient_b0  vgg16 # convnext_tiny
 do
-for variance_conservation in True False 
+for method in normal  # cls ins
 do
+
     python labs/evaluate_attribution/run.py \
             --encoder $encoder \
             --data-path $data_path \
@@ -27,8 +22,7 @@ do
             --input-attrib $input_attrib \
             --p-value-threshold $p_value_threshold \
             --quantile $quantile \
-            --layer-ratio $layer_ratio \
-            --variance-conservation $variance_conservation
-done 
+            --layer-ratio $layer_ratio
+
 done 
 done 

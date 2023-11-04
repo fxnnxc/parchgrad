e28b5291789e2d6ea7da4f0bc89dc40f9d90e336
fxnnxc <bumjin@kaist.ac.kr>
2023-11-04 19:17:54 +0900

    complete the parchgrad

--- labs/evaluate_attribution/custom_filter.py
+++ labs/evaluate_attribution/custom_filter.py
@@ -24,15 +24,17 @@ def f2(flag):
     return True 
 
 
-def f3(flag):
+def no_variance_filter(flag, method, models):
     if not flag.success:
         return False 
-    elif flag.input_attrib not in ["grad", 'ig']:
+    elif flag.input_attrib not in [method]:
         return False 
-    elif flag.layer_ratio not in [1.0]:
+    elif flag.layer_ratio not in [1.0, 0.9, 0.5, 0.3]:
+        return False
+    elif flag.quantile not in [0.05, 0.1]:
         return False
     elif flag.method not in ["cls"]:
         return False 
-    elif flag.encoder not in ["resnet18",]:
+    elif flag.encoder not in models:
         return False 
     return True 
---
+++ shells/no_variance_conservation/ig.sh
@@ -0,0 +1,33 @@
+
+# 
+data_path=/data/ImageNet1k
+bbox_path=/data/ILSVRC2012_bbox_val
+
+input_attrib=ig
+alpha=1e-10 # not used because of quantile 
+p_value_threshold=0.05
+method=cls
+
+export MKL_NUM_THREADS=2
+export NUMEXPR_NUM_THREADS=2
+export OMP_NUM_THREADS=2
+
+
+for quantile in 0.05 0.1 
+do 
+for layer_ratio in 1.0 0.9 0.5 0.3 
+do 
+    save_name=$variance_conservation'_'$quantile'_'$layer_ratio
+    python labs/evaluate_attribution/run.py \
+            --encoder $encoder \
+            --data-path $data_path \
+            --bbox-path $bbox_path \
+            --method $method \
+            --input-attrib $input_attrib \
+            --p-value-threshold $p_value_threshold \
+            --quantile $quantile \
+            --layer-ratio $layer_ratio \
+            --variance-conservation $variance_conservation \
+            --save-name $save_name
+done 
+done 
